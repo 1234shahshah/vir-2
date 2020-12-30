6da9a45eef98fe625152047af5d8f8ed281c873c
1234shahshah <71225198+1234shahshah@users.noreply.github.com>
2020-12-30 11:01:31 -0600

    Add files via upload

---
+++ js/Food.js
@@ -0,0 +1,36 @@
+class Food 
+{
+constructor()
+{
+var foodStock
+var lastFed
+var milk1 
+  }
+  
+  milk=loadImage("milk.png")
+  mik=loadImage("mik.png")
+  getFoodStock()
+  {
+
+  }
+
+  updateFoodStock()
+  {
+
+  }
+
+  deductFood()
+  {
+
+  }
+  display()
+  {
+      if (keyDown(UP_ARROW))
+      {
+        milk1=addImage("mik",milk)
+      }else
+      {
+       milk1=addImage("mi",mik)
+      }
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,80 @@
+//Create variables here
+var dog1
+var happyDog
+var database
+var foods 
+var foodStock
+var fedTime
+var lastFed
+var foodObj
+var gameState
+var changeState
+var readState
+var garden
+var bedroom
+var bathroom
+
+function preload()
+{
+  //load images here
+  dog=loadImage("images/dogImg.png")
+  happyDog=loadImage("images/dogImg1.png")
+  garden=loadImage("images/garden.png")
+  bedroom=loadImage("images/bedroom.png")
+  bathroom=loadImage("images/bathroom.png")
+
+
+}
+
+function setup() {
+	createCanvas(500, 500);
+  
+database=firebase.database()
+readState=database
+dog1 = createSprite(250,250)
+  dog1.addImage("d",dog)
+  foodStock=database.ref("Food")
+  foodStock.on("value",readStock)
+  foo = new Food(250,250)
+}
+
+
+function draw() {  
+background(46,139,87)
+if (keyDown(UP_ARROW))
+{
+  foodStock=foodStock-1
+  dog1.addImage("do",happyDog)
+}
+if (keyDown("A_ARROW"))
+{
+
+} 
+if (keyDown("Q_ARROW"))
+{
+  
+}
+  drawSprites();
+
+  //add styles here
+
+}
+function readStock(data)
+{
+  foods=data.val()
+}
+
+function writeStock(x)
+{
+  if(x<=0)
+  {
+    x=0
+  } else{
+    x=x-1
+  }
+  database.ref("/").update({Food:x})
+}
+
+
+
+
